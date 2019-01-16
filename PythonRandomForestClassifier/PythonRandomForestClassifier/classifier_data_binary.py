ddd596dda3d73e1cb6a4f071ec1c0f3adefa9c55
it102018 <damian.dudek@interia.eu>
2019-01-16 23:54:35 +0100

    Add print output

--- PythonRandomForestClassifier/PythonRandomForestClassifier/classifier_data_binary.py
+++ PythonRandomForestClassifier/PythonRandomForestClassifier/classifier_data_binary.py
@@ -16,30 +16,58 @@ def main():
     df = df.apply(le.fit_transform)
 
     classLabelAttribute = ["class"]
-    observationsLabelAttribute = ["top-left-square", "top-middle-square", "top-right-square", "top-right-square", "middle-middle-square", "middle-right-square", "bottom-left-square", "bottom-middle-square", "bottom-right-square"]
+    observationsLabelAttribute = ["top-left-square", 
+                                  "top-middle-square", 
+                                  "top-right-square", 
+                                  "top-right-square", 
+                                  "middle-middle-square", 
+                                  "middle-right-square", 
+                                  "bottom-left-square", 
+                                  "bottom-middle-square", 
+                                  "bottom-right-square"]
 
     target = df[classLabelAttribute]
     data = df[observationsLabelAttribute]
 
-    train_size = 0.5
-    test_size = 0.5
-    random_state = 0
+    train_size = 0.75
+    test_size = 0.25
+    shuffle = True;
 
-    train_x, test_x, train_y, test_y = train_test_split(data, target, train_size = train_size, test_size = test_size, random_state = random_state)
+    train_x, test_x, train_y, test_y = train_test_split(data, 
+                                                        target, 
+                                                        train_size = train_size, 
+                                                        test_size = test_size,
+                                                        shuffle = shuffle)
 
-    n_estimators = 10
+    n_estimators = 500
+    criterion = 'entropy'
 
-    rfc = RandomForestClassifier(n_estimators = n_estimators)
+    rfc = RandomForestClassifier(n_estimators = n_estimators, criterion = criterion)
 
     trained_model = rfc.fit(train_x, train_y.values.ravel())
 
-    predictions_train = trained_model.predict(train_x)
+    #predictions_train = trained_model.predict(train_x)
     predictions_test = trained_model.predict(test_x)
 
-    print('Train Accuracy: {0}'.format(accuracy_score(train_y, predictions_train)))
-    print('Test Accuracy: {0}'.format(accuracy_score(test_y, predictions_test)))
+    print("")
+    
+    print("Parametry dla funkcji train_test_split: ")
+    print('train_size: {0} '.format(train_size), end='')
+    print('test_size: {0} '.format(test_size), end='')
+    print('shuffle: {0} '.format(shuffle), end='')
+    
+    print("")
 
-    print(pd.crosstab(index=test_y.values.ravel(), columns=predictions_test, rownames=["actual"], colnames=["predictions"]))
+    print("Parametry dla funkcji RandomForestClassifier:")
+    print('n_estimators: {0} '.format(n_estimators), end='')
+    print('criterion: {0} '.format(criterion), end='')
+
+    print("")
+
+    print(pd.crosstab(index=test_y.values.ravel(), columns=predictions_test, rownames=["Faktyczny"], colnames=["Przewidywania"]))
+
+    #print('Wynik klasyfikacji dla zbioru uczącego: {0}'.format(accuracy_score(train_y, predictions_train)))
+    print('Wynik klasyfikacji: {0}'.format(accuracy_score(test_y, predictions_test)))
 
 if __name__ == "__main__":
     main()
--- PythonRandomForestClassifier/PythonRandomForestClassifier/classifier_data_multi_class.py
+++ PythonRandomForestClassifier/PythonRandomForestClassifier/classifier_data_multi_class.py
@@ -23,25 +23,45 @@ def main():
     target = df[classLabelAttribute]
     data = df[observationsLabelAttribute]
 
-    train_size = 0.5
-    test_size = 0.5
-    random_state = 0
+    train_size = 0.75
+    test_size = 0.25
+    shuffle = True;
 
-    train_x, test_x, train_y, test_y = train_test_split(data, target, train_size = train_size, test_size = test_size, random_state = random_state)
-    
-    n_estimators = 10
+    train_x, test_x, train_y, test_y = train_test_split(data, 
+                                                        target, 
+                                                        train_size = train_size, 
+                                                        test_size = test_size,
+                                                        shuffle = shuffle)
 
-    rfc = RandomForestClassifier(n_estimators = n_estimators)
+    n_estimators = 500
+    criterion = 'entropy'
+
+    rfc = RandomForestClassifier(n_estimators = n_estimators, criterion = criterion)
 
     trained_model = rfc.fit(train_x, train_y.values.ravel())
 
-    predictions_train = trained_model.predict(train_x)
+    #predictions_train = trained_model.predict(train_x)
     predictions_test = trained_model.predict(test_x)
 
-    print('Train Accuracy: {0}'.format(accuracy_score(train_y, predictions_train)))
-    print('Test Accuracy: {0}'.format(accuracy_score(test_y, predictions_test)))
+    print("")
+    
+    print("Parametry dla funkcji train_test_split: ")
+    print('train_size: {0} '.format(train_size), end='')
+    print('test_size: {0} '.format(test_size), end='')
+    print('shuffle: {0} '.format(shuffle), end='')
+    
+    print("")
 
-    print(pd.crosstab(index=test_y.values.ravel(), columns=predictions_test, rownames=["actual"], colnames=["predictions"]))
+    print("Parametry dla funkcji RandomForestClassifier:")
+    print('n_estimators: {0} '.format(n_estimators), end='')
+    print('criterion: {0} '.format(criterion), end='')
+
+    print("")
+
+    print(pd.crosstab(index=test_y.values.ravel(), columns=predictions_test, rownames=["Faktyczny"], colnames=["Przewidywania"]))
+
+    #print('Wynik klasyfikacji dla zbioru uczącego: {0}'.format(accuracy_score(train_y, predictions_train)))
+    print('Wynik klasyfikacji: {0}'.format(accuracy_score(test_y, predictions_test)))
 
 if __name__ == "__main__":
     main()
